d573bc4dbc8db272ad066e4a1448e486b55fa3b5
Simón Villafañe <simonvillafanee@gmail.com>
2025-12-27 09:16:58 -0300

    feat(+kata): int32 to IPv4

--- main.lua
+++ main.lua
@@ -1,22 +1,14 @@
-local kata = {
-  hex = {
-    '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-  }
-}
+local solution = {}
 
-function kata.rgb(r, g, b)
-  r = math.max(0, math.min(255, r))
-  g = math.max(0, math.min(255, g))
-  b = math.max(0, math.min(255, b))
-
-  local function toHex(n)
-    local high = math.floor(n / 16) + 1
-    local low = (n % 16) + 1
-    return kata.hex[high] .. kata.hex[low]
-  end
-
-  return toHex(r) .. toHex(g) .. toHex(b)
+-- 256 = 2^8
+-- 0+8+16+24 = 32 bits
+function solution.int32_to_ip(int32)
+  return ("%d.%d.%d.%d"):format(
+    math.floor(int32 / 2 ^ 24) % 256, -- 24 = 8+8+8
+    math.floor(int32 / 2 ^ 16) % 256, -- 16 = 8+8
+    math.floor(int32 / 2 ^ 8) % 256,  -- 8  = 8
+    int32 % 256                       -- 0
+  )
 end
 
-return kata
+return solution
